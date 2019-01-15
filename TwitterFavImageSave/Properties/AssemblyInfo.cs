26cb5ba39eae0fca0f047aeadc076d78ca04a774
Ryosuke UCHIYAMA <uchi63rs@yahoo.co.jp>
2019-01-15 21:36:27 +0900

    2019.1.15.0 Release ver

--- TwitterFavImageSave/Properties/AssemblyInfo.cs
+++ TwitterFavImageSave/Properties/AssemblyInfo.cs
@@ -8,11 +8,11 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TwitterFavImageSave")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Help quick save images in your favarites.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("minato project")]
 [assembly: AssemblyProduct("TwitterFavImageSave")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright ©  minato 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2019.1.15.0")]
+[assembly: AssemblyFileVersion("2019.1.15.0")]
